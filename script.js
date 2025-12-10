517a3cb9304b35b0c2f5e00dfeebbbb200e30174
SweetSweetGwendoline <8bitblip@googlemail.com>
2025-12-10 17:24:54 +0100

    Remove last updated date from script.js
    Remove hardcoded last updated date from script.

--- script.js
+++ script.js
@@ -44,8 +44,6 @@ async function loadPresskit() {
   document.getElementById('studio').textContent = `${s.name}`;
   const contacts = s.contact || {};
   document.getElementById('contacts').innerHTML = Object.entries(contacts).filter(([k, v]) => v).map(([k, v]) => `<li><a class='brand' target='_blank' href='${v}'>${k}</a></li>`).join('');
-
-  document.getElementById('lastUpdated').textContent = "Last Updated: 2025-08-30";
 }
 
 loadPresskit();
